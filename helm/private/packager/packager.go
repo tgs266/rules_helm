89822c1f48a0c58e91e5149b113f0ff766f679a4
Tucker Siegel <tgsiegel@umd.edu>
2023-09-13 19:49:42 -0400

    Update packager.go

--- helm/private/packager/packager.go
+++ helm/private/packager/packager.go
@@ -423,6 +423,8 @@ func main() {
 	// Build the helm package
 	command := exec.Command(path.Join(cwd, args.helm), "package", ".", "--debug")
 	command.Dir = tmp_path
+	command.Stdout = os.Stdout 
+	command.Stderr = os.Stdout
 	out, err := command.Output()
 	if err != nil {
 		log.Println(string(out))
